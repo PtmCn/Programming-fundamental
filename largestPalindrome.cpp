a5765c59f4452322e32e03125389d10b459f00f4
PtmCn <69117387+PtmCn@users.noreply.github.com>
2020-08-30 05:13:23 -0700

    week 3

---
+++ largestPalindrome.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main ()
+{
+	int x,y;
+	int product, revProd, reverse, max = 0;
+	
+	for (x=100;x<1000;x++)
+	{
+		for (y=100;y<1000;y++)
+		{
+			product = x*y;
+			
+			revProd = product;
+			reverse = 0;
+			
+			while (revProd>0)
+			{
+				reverse = reverse*10+ revProd%10;
+				revProd /=10;
+			}
+			if (reverse == product)
+			{
+				if (product> max)
+				{
+					max =product;
+				}
+			}
+		}
+	}
+	printf ("%d", max);
+return 0;
+}
