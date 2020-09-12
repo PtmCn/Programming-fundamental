ee724b5fcf0be254cf481aac41a950159f5850ce
PtmCn <69117387+PtmCn@users.noreply.github.com>
2020-09-13 01:03:57 +0700

    week5

--- OnlyCptl.cpp
+++ OnlyCptl.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int n;
-	char str[20] = "abCDEfGHiJk";
-	for (n=0;n<=20;n++)
+	int n,b;
+	char str[1000];
+	scanf ("%[^\n]s", str);
+	b=strlen(str);
+	for (n=0;n<=b;n++)
 	{
 		if (str[n]>=65 && str[n]<=90)
 		{	
