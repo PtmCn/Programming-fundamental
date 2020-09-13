bdae9967770e0f5465b20b7c6af64b449845fd29
PtmCn <69117387+PtmCn@users.noreply.github.com>
2020-09-13 07:24:54 -0700

    week5

---
+++ PE16 PowerDigitSum.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{    
+	int num,sum = 2;
+	printf ("Enter number:");
+	scanf ("%d", &num);
+	for (int n=1;n<num;n++)
+	{
+		sum*=2;
+	}
+	
+	int  dgsum = 0, rem ; 
+	while (sum/10 != 0)
+	{
+		while (sum!=0)
+		{
+			rem= sum%10;
+			dgsum += rem;
+			sum=sum/10;
+		}
+		printf ("%d", dgsum);
+	}
+    return 0;
+}
