f6f8011da38cc3d3e3665b0b92040be5fce98a39
PtmCn <69117387+PtmCn@users.noreply.github.com>
2020-10-11 18:57:28 +0700

    week 9

---
+++ fibonacciTriangle.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    int firstnum=1,secondnum=1,sum=0,n,i,j;
+    printf ("Enter the number of lines:");
+    scanf ("%d", &n);
+    printf ("Output:\n");
+    Sleep(1000);
+    for ( j = 1;j<=n;j++)
+    {
+        for ( i = 1;i<=j;i++)
+        {
+            printf ("%d\t",firstnum);
+            sum = firstnum+secondnum;
+            firstnum = secondnum;
+            secondnum = sum;
+            if(i == j){printf ("\n");}
+        }
+    }
+    return 0;
+}
